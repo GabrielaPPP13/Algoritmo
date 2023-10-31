c3611c206aa9bc1c8ad749768883c5af419d912c
GabrielaPPP13 <cngabriela20@gmail.com>
2023-10-31 00:47:52 -0600

    Tercer Commit

--- src/Algoritmo.java
+++ src/Algoritmo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Collections;
+//import java.util.Collections;
 
 public class Algoritmo {
     public static void main(String[] args) {
